64070f0eb6036553e0805cad761158ddc1a9ccac
Mark Brooks <Markabrooks@gmail.com>
2017-12-31 18:34:07 -0700

    Section 1 12to20: Complete

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAEZ33w6N7oYUzRznksRjtDIjSrIyzMmq4';
+
 // Create a new component.  This component should produce
 // some HTML
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's generated HTML and put it
