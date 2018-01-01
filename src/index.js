1fc9686bc6a93dec6e17a149e2f250345fb0349e
Mark Brooks <Markabrooks@gmail.com>
2018-01-01 11:18:44 -0700

    Section 2: 21-26 Complete

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyAEZ33w6N7oYUzRznksRjtDIjSrIyzMmq4';
 
-// Create a new component.  This component should produce
-// some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos }); // this.setState({ videos: videos })
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
-// Take this component's generated HTML and put it
-// on the page {in the DOM}
 ReactDOM.render(<App />, document.querySelector('.container'));
