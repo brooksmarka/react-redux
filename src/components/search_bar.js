6d18085e6a563e25e72810780d03fbfe752acfa4
Mark Brooks <Markabrooks@gmail.com>
2018-01-02 11:06:32 -0700

    Section 2: Complete

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,14 +6,21 @@ class SearchBar extends Component {
 
     this.state = { term: '' };
   }
+
   render() {
     return (
-      <div>
+      <div className="search-bar">
         <input
           value = {this.state.term}
-          onChange={event => this.setState({ term: event.target.value })} />
+          onChange={event => this.onInputChange(event.target.value)} />
       </div>
     );
+  }
+
+  onInputChange(term){
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+
   }
 }
 
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
   console.log(video);
   const imageUrl = video.snippet.thumbnails.default.url;
 
   return (
-    <li className="list-group-item">
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
           <img className="media-object" src={imageUrl} />
